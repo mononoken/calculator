e84e5ccf5be0205c172a1b838396d25fb7212c88
mononoken <ken.maeshima@pm.me>
2021-11-26 09:55:19 -0600

    Clear button is working.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let calculation = 0;
+
 const add = (num1, num2) => (num1 + num2);
 
 const subtract = (num1, num2) => (num1 - num2);
@@ -21,4 +23,18 @@ function operate(num1, num2, operator) {
       return divide(num1, num2);
       break;
   }
-} 
+}
+
+let calcScreen = document.getElementById('calc-screen');
+let clearBtn = document.getElementById('clear-btn');
+
+function clearScreen() {
+  calculation = 0;
+  calcScreen.textContent = calculation.toString();
+}
+
+clearBtn.addEventListener('click', event => {
+  clearScreen();
+});
+
+// Was writing function to clear screen.
