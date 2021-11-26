55bdd104cd6b5d0741eee9e573f23b8dc955bb46
mononoken <ken.maeshima@pm.me>
2021-11-26 11:40:23 -0600

    Operation buttons initially working.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
 let calculation = 0;
 let operand = 0;
-let operation; 
+let operation = null; 
+
+const calcScreen = document.getElementById('calc-screen');
+const clearBtn = document.getElementById('clear-btn');
+
+const zeroBtn = document.getElementById('zero-btn');
+const oneBtn = document.getElementById('one-btn');
+const twoBtn = document.getElementById('two-btn');
+const threeBtn = document.getElementById('three-btn');
+const fourBtn = document.getElementById('four-btn');
+const fiveBtn = document.getElementById('five-btn');
+const sixBtn = document.getElementById('six-btn');
+const sevenBtn = document.getElementById('seven-btn');
+const eightBtn = document.getElementById('eight-btn');
+const nineBtn = document.getElementById('nine-btn');
+
+const plusBtn = document.getElementById('plus-btn');
+const minusBtn = document.getElementById('minus-btn');
+const multiplyBtn = document.getElementById('multiply-btn');
+const divideBtn = document.getElementById('divide-btn');
+const equalsBtn = document.getElementById('equals-btn');
 
 const add = (num1, num2) => (num1 + num2);
 
@@ -26,19 +46,6 @@ function operate(num1, num2, operator) {
       break;
   }
 }
-
-const calcScreen = document.getElementById('calc-screen');
-const clearBtn = document.getElementById('clear-btn');
-const zeroBtn = document.getElementById('zero-btn');
-const oneBtn = document.getElementById('one-btn');
-const twoBtn = document.getElementById('two-btn');
-const threeBtn = document.getElementById('three-btn');
-const fourBtn = document.getElementById('four-btn');
-const fiveBtn = document.getElementById('five-btn');
-const sixBtn = document.getElementById('six-btn');
-const sevenBtn = document.getElementById('seven-btn');
-const eightBtn = document.getElementById('eight-btn');
-const nineBtn = document.getElementById('nine-btn');
 
 function refreshScreen() {
   calcScreen.textContent = calculation;
@@ -102,4 +109,37 @@ nineBtn.addEventListener('click', event => {
 
 zeroBtn.addEventListener('click', event => {
   clickNumBtn(0);
+});
+
+plusBtn.addEventListener('click', event => {
+  operation = 'addition';
+  operand = calculation;
+  calculation = 0;
+  refreshScreen();
+});
+
+minusBtn.addEventListener('click', event => {
+  operation = 'subtraction';
+  operand = calculation;
+  calculation = 0;
+  refreshScreen();
+});
+
+multiplyBtn.addEventListener('click', event => {
+  operation = 'multiplication';
+  operand = calculation;
+  calculation = 0;
+  refreshScreen();
+});
+
+divideBtn.addEventListener('click', event => {
+  operation = 'division';
+  operand = calculation;
+  calculation = 0;
+  refreshScreen();
+});
+
+equalsBtn.addEventListener('click', event => {
+  calculation = operate(operand, calculation, operation);
+  refreshScreen();
 });
